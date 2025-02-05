17422f40539e0c5ab14c34dad07ee4801a2fda17
harsha0072 <vhs06148@gmail.com>
2025-02-05 19:00:38 +0530

    API Authentication

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,14 +1,13 @@
-using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]  //api/users
-public class UsersController(DataContext context) : ControllerBase
+[Authorize]
+public class UsersController(DataContext context) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
